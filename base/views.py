3a6d9fbf5c838d5249f6d5042e8fc7430e93694d
kurukimi <kimi.kuru@hotmail.com>
2022-01-27 18:41:40 +0200

    refactor

--- base/views.py
+++ base/views.py
@@ -58,7 +58,6 @@ def registerPage(request):
     return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
-    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     rooms = Room.objects.filter(
@@ -66,22 +65,21 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    room_count = rooms.count()
-
-    if is_ajax:
-        html = render_to_string(template_name="base/partial_room.html", context={'rooms' : rooms, 'room_count':room_count})
-        dataDict = {'rooms' : html}
-        return JsonResponse(dataDict, safe=False)
 
     topics = Topic.objects.all()
-    
-    context = {'rooms' : rooms, 'topics' : topics, 'room_count':room_count}
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))
+
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -89,9 +87,10 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room':room, 'room_messages': room_messages}
+    context = {'room':room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -131,3 +130,17 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', context)
+
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Not your message')
+    
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
